008967a4b933bc605fbdeb93a4891c50fb6cea76
hhn1n15 <hainhbkhn@gmail.com>
2022-09-10 13:31:29 -0400

    minor tweak

--- plot.py
+++ plot.py
@@ -3,6 +3,20 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy import stats
 import os
+import seaborn as sns
+import matplotlib as mpl
+import argparse
+
+
+cmap = plt.get_cmap('tab10')
+
+mpl.rcParams['pdf.fonttype'] = 42
+mpl.rcParams['ps.fonttype'] = 42
+
+SMALL_SIZE = 15
+
+mpl.rc('font', size=SMALL_SIZE)
+mpl.rc('axes', titlesize=SMALL_SIZE)
 
 def lpf(arr, weight=0.8):
   filtered_arr = arr
@@ -11,25 +25,57 @@ def lpf(arr, weight=0.8):
   return filtered_arr
 #----------------------- config here ------------------------#
 
-task = 'house_building_4'
-# algo = 'DQN'
-algo = 'SDQfD'
-num_expert = 25
+parser = argparse.ArgumentParser()
+parser.add_argument('--file', type=str)
+parser.add_argument('--task', type=str, default='block_stacking', help="'1l2b2r','block_stacking','house_building_1','house_building_2','house_building_4")
+parser.add_argument('--algo', type=str, default='DQN', help="'DQN','SDQfD'")
+parser.add_argument('--show', type=int, default=1)
+args = parser.parse_args()
+
+num_experts_lookup = {'1l2b2r': 15,
+                      'block_stacking': 5,
+                      'house_building_1': 5,
+                      'house_building_2': 5,
+                      'house_building_4': 25}
+
+task = args.task
+algo = args.algo
+num_expert = num_experts_lookup[args.task]
+
+if algo in ['DQN']:
+  sns.set_style('darkgrid')
+else:
+  sns.set_style('whitegrid')
+
+# label
+# SDQfD-100
+# SDQfD-5
+# LEO-SDQfD-5
+# LEO-SDQfD-5 (Perfect)
 
 #------------------------------------------------------------#
 parent_folder = f'new_{task}'
+# color_list = [
+#             'g', 
+#             'purple',
+#             'b',
+#             'y', 
+#             'r'
+#             ]
+
 color_list = [
-            'g', 
-            'purple',
-            'b',
-            'r'
+            cmap.colors[0],
+            cmap.colors[1],
+            cmap.colors[2],
+            cmap.colors[3],
+            cmap.colors[4],
             ]
 
 label_list = [
-            num_expert,
-            100,
-            num_expert,
-            num_expert
+            f"{algo}-{num_expert}",
+            f"{algo}-100",
+            f"LEO-{algo}-{num_expert}",
+            f"LEO-{algo}-{num_expert} (Perfect)",
 ]
 # folder_list = os.listdir(parent_folder)
 folder_list = [
@@ -41,20 +87,21 @@ folder_list = [
 
 if (task == 'house_building_4'):
   color_list.append('y')
-  label_list.append(num_expert)
+  # label_list.append(num_expert)
   folder_list.append(f"G-{algo}_equi_Classifier_{num_expert}")
+  label_list.append(f"LEO-{algo}-{num_expert} (Equi)",)
 
 file_name = 'info/eval_rewards.npy'
 line_list = []
 
 for id, folder_name in enumerate(folder_list):
-  print(folder_name)
+  # print(folder_name)
   avg = []
   sub_folder_list = os.listdir(os.path.join(parent_folder, folder_name))
 
   for i, sub_folder in enumerate(sub_folder_list):
     arr = np.load(os.path.join(parent_folder, folder_name, sub_folder, file_name), allow_pickle=True)
-    print(len(arr))
+    # print(len(arr))
     arr = arr[0:len(arr)-1]
     avg.append([])
     for j in range(len(arr)):
@@ -65,17 +112,19 @@ for id, folder_name in enumerate(folder_list):
   std_arr = stats.sem(avg, axis=0)
 
   x = np.arange(0.5, (len(arr)+1)*0.5, 0.5)
-  plt.plot(x, plot_avg, color=color_list[id], linewidth=3,label=label_list[id])
+  plt.plot(x, plot_avg, color=color_list[id], linestyle='solid', linewidth=2, label=label_list[id])
   line_list.append(plot_avg)
-  plt.fill_between(x, plot_avg - std_arr, plot_avg + std_arr, color=color_list[id], alpha=0.3)
-plt.xlim([0,1])
-plt.xticks(np.arange(0, 0.5*(len(arr)+1), 2.5)) 
-plt.yticks(np.arange(0, 1.1, 0.2)) 
-# plt.legend(line_list, folder_list)
-plt.legend(loc='lower left')
+  plt.fill_between(x, plot_avg - std_arr, plot_avg + std_arr, color=color_list[id], alpha=0.15)
+# plt.xlim([0,1])
+# plt.ylim([0,25])
+# plt.xticks(np.arange(0, 0.5*(len(arr)+1), 2.5)) 
+# plt.yticks(np.arange(0, 1.1, 0.2)) 
+# plt.legend(line_list, folder_list)plt.legend(loc='upper left')
+plt.legend()
 # plt.title(parent_folder)
 plt.tight_layout()
-plt.grid()
-# plt.show()
-plt.savefig(os.path.join('Figures', f'{parent_folder}_{algo}.png'),dpi=600)
-
+# plt.grid()
+if args.show == 1:
+  plt.show()
+else:
+  plt.savefig(os.path.join('Figures', f'{parent_folder}_{algo}.png'),dpi=600)
